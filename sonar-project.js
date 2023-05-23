8b3071707ff771597048261143bd1f4d273589b8
Ajay9991 <105283024+Ajay9991@users.noreply.github.com>
2023-05-23 11:26:51 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,14 +1,14 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.21.27.155:9000/',
+  serverUrl: 'http://3.19.241.31:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
 	    'sonar.login': 'sqa_79aadaf277c7cf51049c0b76cce8e27bc8b4fd51',
 	    //'sonar.login': 'admin',
-	    //'sonar.password': 'devops',
+	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
